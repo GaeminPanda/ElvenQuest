4541b81f9ef0f9247ce9fdc919e41ed5494a8e39
Jonathan <1614531@mukilteo.wednet.edu>
2022-03-30 10:12:51 -0700

    healthbar (broken)

---
+++ Assets/Jonathan/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    public float Hitpoints;
+    public float MaxHitpoints = 20;
+    public HealthbarBehavior Healthbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hitpoints = MaxHitpoints;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Hitpoints -= damage;
+
+        if (Hitpoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
