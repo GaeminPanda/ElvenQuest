ccc6729699ed2ebba8a3e609dd2446127f96ecfe
MN <1709731@mukilteo.wednet.edu>
2022-04-13 08:25:47 -0700

    credits done

--- Assets/Jonathan/Menu/MainMenu.cs
+++ Assets/Jonathan/Menu/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
     public string Scenechange;
    public void PlayGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void OnCredits()
