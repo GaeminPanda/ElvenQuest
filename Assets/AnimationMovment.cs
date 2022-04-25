d3d212a18969c93f84f3ec38be6a7a839c1bbd40
MN <1709731@mukilteo.wednet.edu>
2022-04-25 10:10:27 -0700

    particals

---
+++ Assets/AnimationMovment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMovment : MonoBehaviour
+{
+    bool grounded = false;
+
+    //references
+    Rigidbody2D rb2;
+    Spriterenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = gameobject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
