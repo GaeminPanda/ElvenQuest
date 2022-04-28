513f3efa8bcca8a12653809e0311320a436c9072
Jonathan <1614531@mukilteo.wednet.edu>
2022-04-28 09:51:37 -0700

    present

--- Assets/Jonathan/Scripts/Cage.cs
+++ Assets/Jonathan/Scripts/Cage.cs
@@ -15,4 +15,11 @@ public class Cage : MonoBehaviour
     {
         
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Community")
+        {
+            
+        }
+    }
 }
