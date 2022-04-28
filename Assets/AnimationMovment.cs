30955f5cc91b90f7964c87ef8c04e24949663681
Jonathan <1614531@mukilteo.wednet.edu>
2022-04-28 10:17:29 -0700

    start tilemaps

--- Assets/AnimationMovment.cs
+++ Assets/AnimationMovment.cs
@@ -9,12 +9,14 @@ public class AnimationMovment : MonoBehaviour
     //references
     Rigidbody2D rb2;
     SpriteRenderer sr;
+    Animator a;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2 = gameObject.GetComponent<Rigidbody2D>();
         sr = gameObject.GetComponent<SpriteRenderer>();
+        a = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,15 +24,24 @@ public class AnimationMovment : MonoBehaviour
     {
         float horizvalue = Input.GetAxis("Horizontal");
 
+        if (horizvalue == 0)
+        {
+            a.SetBool("Moving", false);
+        }
+        else
+        {
+            a.SetBool("Moving", true);
+        }
+
         rb2.velocity = new Vector2(horizvalue * 2, rb2.velocity.y);
         
         if (horizvalue < 0)
         {
-            sr.flipX = true;
+            sr.flipX = false;
         }
         else
         {
-            sr.flipX = false;
+            sr.flipX = true;
         }
 
         grounded = Physics2D.BoxCast(transform.position, new Vector2(0.1f, 0.1f), 0, Vector2.down, 1, LayerMask.GetMask("Ground"));
