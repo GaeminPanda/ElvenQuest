386dcd300c7044517f65a890825b38197fd89674
MN <1709731@mukilteo.wednet.edu>
2022-04-28 08:06:36 -0700

    animation for player continued

--- Assets/AnimationMovment.cs
+++ Assets/AnimationMovment.cs
@@ -14,11 +14,30 @@ public class AnimationMovment : MonoBehaviour
     void Start()
     {
         rb2 = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizvalue = Input.GetAxis("Horizontal");
+
+        rb2.velocity = new Vector2(horizvalue * 2, rb2.velocity.y);
         
+        if (horizvalue < 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+
+        grounded = Physics2D.BoxCast(transform.position, new Vector2(0.1f, 0.1f), 0, Vector2.down, 1, LayerMask.GetMask("Ground"));
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2.velocity = new Vector2(rb2.velocity.x, 6);
+        }
     }
 }
